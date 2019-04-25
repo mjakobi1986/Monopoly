e203bf21ae483f2bba2a6c083e3e7f6607ed1c64
mjakobi1986 <21143350+mjakobi1986@users.noreply.github.com>
2019-04-25 17:13:05 +0200

    Create FieldPanel.java

---
+++ FieldPanel.java
@@ -0,0 +1,25 @@
+
+public class FieldPanel extends JPanel {
+	
+	public enum alignment {
+		STREET_SOUTH, STREET_WEST, STREET_NORTH, STREET_EAST,
+		FIELD_SOUTH, FIELD_WEST, FIELD_NORTH, FIELD_EAST,
+		CORNER,
+		JAIL;
+		
+	}
+	
+	private int fieldID;
+	
+	
+	public FieldPanel() {
+		super();
+	}
+	
+	private void setFieldLayout(int align) {
+		GridBagLayout gbl = new GridBagLayout();
+		
+		this.se
+	}
+	
+}
