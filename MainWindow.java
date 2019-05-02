e067ee92a1c5a667e2ddd11ac8aac2bf46fc9b55
mjakobi1986 <21143350+mjakobi1986@users.noreply.github.com>
2019-05-02 13:21:36 +0200

    Create MainWindow.java

---
+++ MainWindow.java
@@ -0,0 +1,12 @@
+public class MainWindow extends JFrame () {
+
+    public MainWindow() {
+        super("Monopoly");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        setSize(new Dimension(700, 700));
+        isResizeable("true");
+        isVisible("true");
+    }
+}
